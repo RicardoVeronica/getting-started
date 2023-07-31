5b21dcf620e88b1b94a5ad1186b29e943cc52c70
ricardoveronica <ricardo_veronica.duran@hotmail.com>
2023-07-31 16:40:47 -0600

    feat(views) adds form for new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,4 +15,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirec_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
