d1790954e3487dc8e3814776464c5d2c65adf894
ricardoveronica <ricardo_veronica.duran@hotmail.com>
2023-08-01 09:01:14 -0600

    feat: adds comments in articles

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # when user gets /articles/
   # redirec to articles controller with index action
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
